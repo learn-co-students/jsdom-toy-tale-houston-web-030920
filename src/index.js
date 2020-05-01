c559af4c6b75827cede4392f3f32512d1e252d03
melkilic <melike180400@gmail.com>
2020-04-30 21:16:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,74 +1,77 @@
 let addToy = false;
-const URL= "http://localhost:3000/toys";
+const URL = "http://localhost:3000/toys";
 
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-let createToy= function(toys){
-  console.log(toys)
-  const toyCollection= document.querySelector('#toy-collection');
-  let cardDiv= document.createElement('div');
-  cardDiv.setAttribute("class","card");
-  toyCollection.append(cardDiv)
-  let name=document.createElement('h2')
-  name.innerHTML=toys.name
-  let img= document.createElement('img')
-  img.src= toys.image
-  img.style.width="100px"
-  img.setAttribute("class","toy-avatar")
-  let pTag=document.createElement("p")
-  pTag.innerHTML=`${toys.likes} Likes`
-  let button= document.createElement("button")
-  button.setAttribute("class","like-btn")
-  button.innerText= "Like <3"
+  let createToy = function (toys) {
+    const toyCollection = document.querySelector('#toy-collection');
+    let cardDiv = document.createElement('div');
+    cardDiv.setAttribute("class", "card");
+    toyCollection.append(cardDiv)
 
-  button.addEventListener('click',function(e){
-    e.preventDefault();
-    fetch(`http://localhost:3000/toys/${toys.id}`,{
-      
-    method:"PATCH",
-      headers: 
-      {
-        "Content-Type": "application/json",
-         Accept: "application/json"
-      },
-      
-      body: JSON.stringify({
-        "likes": toys.likes += 1
+    let name = document.createElement('h2')
+    name.innerHTML = toys.name
+
+    let img = document.createElement('img')
+    img.src = toys.image
+    img.style.width = "100px"
+    img.setAttribute("class", "toy-avatar")
+
+    let pTag = document.createElement("p")
+    pTag.innerHTML = `${toys.likes} Likes`
+
+    let button = document.createElement("button")
+    button.setAttribute("class", "like-btn")
+    button.innerText = "Like <3"
+
+    button.addEventListener('click', function (e) {
+      e.preventDefault();
+      fetch(`http://localhost:3000/toys/${toys.id}`, {
+
+        method: "PATCH",
+        headers:
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+
+        body: JSON.stringify({
+          "likes": toys.likes += 1
+        })
       })
+        .then(function (e) {
+          return e.json()
+        })
+        .then(function (e) {
+          pTag.innerHTML = `${e.likes} Likes`
+        })
+
+
+
+
     })
-      .then(function(e){
-        return e.json()
-      })
-      .then(function(e){
-        pTag.innerHTML= `${e.likes} Likes` 
-      })
-  
-   
+    cardDiv.append(name, img, pTag, button)
+  }
 
 
+
+  fetch("http://localhost:3000/toys", {
+    method: "GET"
   })
-  cardDiv.append(name,img,pTag,button)
-}
+    .then(function (r) {
+      return r.json()
+    })
+    .then(function (toys) {
+      for (let i = 0; i < toys.length; i++) {
+        createToy(toys[i]);
+      }
+      // render(toys);
+    })
 
 
 
-  fetch("http://localhost:3000/toys",{
-    method:"GET"
-  })
-  .then(function(r){
-  return r.json()
-  })
-  .then(function(toys){
-    for(let i=0; i<toys.length; i++){
-      createToy(toys[i]);
-    }
-    // render(toys);
-  })
-
-
-  
 
   const toyFormContainer = document.querySelector(".container");
   const addBtn = document.querySelector("#new-toy-btn");
@@ -77,14 +80,18 @@ let createToy= function(toys){
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
-      let form= document.querySelector(".add-toy-form")
-      form.addEventListener("submit",function(e){
+      let form = document.querySelector(".add-toy-form")
+
+
+      form.addEventListener("submit", function (e) {
         e.preventDefault();
-        let nameInput= document.querySelector(".name-input")
-        let imageInput= document.querySelector(".image-input")
-        fetch("http://localhost:3000/toys",{ 
-          method:"POST",
-          headers:{
+        let nameInput = document.querySelector(".name-input")
+        let imageInput = document.querySelector(".image-input")
+
+
+        fetch("http://localhost:3000/toys", {
+          method: "POST",
+          headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
           },
@@ -95,15 +102,15 @@ let createToy= function(toys){
           })
 
         })
-          .then(function(response){
+          .then(function (response) {
             return response.json();
           })
 
-          .then(function(e){
+          .then(function (e) {
             createToy(e);
           })
-        })
-      
+      })
+
     } else {
       toyFormContainer.style.display = "none";
     }
