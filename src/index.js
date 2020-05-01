e22790bf5f3e215ce785950fc624ee6b7eb7a515
RafaS92 <manrvaldez..92@gmail.com>
2020-04-30 20:32:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,76 +1,104 @@
 let addToy = false;
+const allToys = "http://localhost:3000/toys";
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-
-  fetch("http://localhost:3000/toys")
-    .then(function (resp) {
-      return resp.json();
-    })
-    .then(function (result) {
-      result.forEach((toy) => {
-        createToys(toy);
-      });
-    });
-
-  function createToys(toy) {
-    let toyarray = document.querySelector("#toy-collection");
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-    let toyH2 = document.createElement("h2");
-    toyH2.innerText = toy.name;
-    card.append(toyH2);
-    let toyImg = document.createElement("img");
-    toyImg.src = toy.image;
-    toyImg.setAttribute("class", "toy-avatar");
-    card.append(toyImg);
-    let toyLikes = document.createElement("p");
-    toyLikes.innerText = toy.likes;
-    card.append(toyLikes);
-    let likeButton = document.createElement("button");
-    likeButton.setAttribute("class", "like-btn");
-    likeButton.append("Like <3");
-    card.append(likeButton);
-    likeButton.addEventListener("click", function (e) {
-      addALike(toy, toyLikes);
-    });
-
-    toyarray.append(card);
-  }
-
-  addBtn.addEventListener("click", (e) => {
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
-
-      let otherForm = document.querySelector(".container");
-      otherForm.addEventListener("submit", function (e) {
-        newToy();
+      //add New Toy event listener
+      let createNewToyForm = document.querySelector(".container");
+      createNewToyForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        addNewToy(e.target);
+        console.log("this worked");
       });
     } else {
       toyFormContainer.style.display = "none";
     }
   });
 
-  function addToy(newToy) {
-    fetch("http://localhost:3000/toys", {
+  fetch(allToys)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (toys) {
+      toys.forEach((toy) => {
+        createToyCard(toy);
+      });
+    });
+
+  function createToyCard(toy) {
+    let toyCollection = document.querySelector("#toy-collection");
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    let toyH2 = document.createElement("h2");
+    toyH2.innerText = toy.name;
+    card.append(toyH2);
+
+    let toyImg = document.createElement("img");
+    toyImg.src = toy.image;
+    toyImg.setAttribute("class", "toy-avatar");
+    card.append(toyImg);
+
+    let toyLikes = document.createElement("p");
+    toyLikes.innerText = toy.likes;
+    card.append(toyLikes);
+
+    let likeButton = document.createElement("button");
+    likeButton.setAttribute("class", "like-btn");
+    card.append(likeButton);
+
+    likeButton.addEventListener("click", function (e) {
+      addALike(toy, toyLikes);
+    });
+
+    toyCollection.append(card);
+  }
+
+  function addNewToy(newToyInput) {
+    fetch(allToys, {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
       body: JSON.stringify({
-        name: newToy.name,
-        image: newToy.image,
+        name: newToyInput.name.value,
+        image: newToyInput.image.value,
         likes: 0,
       }),
     })
       .then(function (response) {
         return response.json();
       })
-      .then(function (result) {
-        console.log(result);
-        createToyCard(result);
+      .then(function (toyObject) {
+        console.log(toyObject);
+        createToyCard(toyObject);
+      });
+  }
+
+  function addALike(toy, toyLikesContainer) {
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        likes: (toy.likes += 1),
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (like) {
+        toyLikesContainer.innerText = toy.likes;
       });
   }
 });
